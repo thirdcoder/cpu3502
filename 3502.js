d455f4fa2bbff83f5b40db2a379d6102ec208bfc
thirdcoder <boris3rd@bk.ru>
2015-07-03 13:32:25 -0700

    Move architectural definitions to arch.js

--- 3502.js
+++ 3502.js
@@ -3,13 +3,8 @@
 const {bts2n, n2bts, N_TO_BT_DIGIT, BT_DIGIT_TO_N} = require('balanced-ternary');
 const {get_trit, slice_trits} = require('trit-getset');
 
-const TRITS_PER_TRYTE = 5;
-const TRYTES_PER_WORD = 2;
-const TRITS_PER_WORD = TRITS_PER_TRYTE*TRYTES_PER_WORD;
-const MAX_TRYTE = +(3**TRITS_PER_TRYTE-1)/2;
-const MIN_TRYTE = -(3**TRITS_PER_TRYTE-1)/2;
+const { TRITS_PER_TRYTE, TRYTES_PER_WORD, TRITS_PER_WORD, MAX_TRYTE, MIN_TRYTE, MEMORY_SIZE } = require('./arch');
 
-const MEMORY_SIZE = 3**(TRITS_PER_WORD);
 let memory = new Int8Array(new ArrayBuffer(MEMORY_SIZE)); // Int8Array is 8-bit signed -129 to +128, fits 5-trit -121 to +121
 
 let pc = 0;

1a9c3095aa670e25f91264e426f93095a0374396
thirdcoder <boris3rd@bk.ru>
2015-07-11 19:17:55 -0700

    Memory-map the tritmapped-terminal canvas display to the lower CPU memory

--- demo.js
+++ demo.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const CPU = require('./3502');
+const {TRITS_PER_TRYTE, TRYTES_PER_WORD} = require('./arch');
+const Triterm = require('tritmapped-terminal');
 
-var cpu = CPU();
+const cpu = CPU();
+global.cpu = cpu;
+
+const VIDEO_TRYTE_ADDRESS_SIZE = 4; // 4 trits in each dimension, xxxx and yyyy
+const MEMORY_MAP = {
+  VIDEO_START: 0, // TODO: start at higher address?
+  VIDEO_END: (3**VIDEO_TRYTE_ADDRESS_SIZE * TRITS_PER_TRYTE)**TRYTES_PER_WORD / TRITS_PER_TRYTE, // '00xxx xyyyy' address -> 'xxxxx' tritmap value
+};
+
+const term = Triterm({
+  addressTryteSize: VIDEO_TRYTE_ADDRESS_SIZE,
+  tritmap: cpu.memory.subarray(MEMORY_MAP.VIDEO_START, MEMORY_MAP.VIDEO_END)
+});
+
 cpu.run();
 

321c21d17eaed7a580f3083dc9fa1a460e8489d4
thirdcoder <boris3rd@bk.ru>
2015-07-11 19:28:34 -0700

    Move test assembly into demo, add assemble() call to CPU (assembles and loads into memory)

--- 3502.js
+++ 3502.js
@@ -22,73 +22,21 @@ class CPU {
     this.stackptr = 0;
     this.flags = 0;
 
-    var lines = [
-      'LDA #$ijk',
-      'LDA #%ii1i0',
-      'LDA #&QF',
-      'NOP A',
-      'NOP #-121',
-      'NOP 29524',
-      'LDA #0',
-      'BNE -1',
-      'BEQ +2',
-      'HALT_N',
-      'HALT_P',
-      'LDA #42',
-      'STA 0',
-
-      'LDA #%00i01',
-      'PTI A',
-
-      'TAX',
-
-      'HALT_Z'
-      /*
-
-        this.memory[x++] = bts2n('10i10'); // operation 10i, addressing mode 1
-        this.memory[x++] = bts2n('11001'); // flag 11, trit 0, compare 0
-        this.memory[x++] = bts2n('00000'); // nop a
-
-        this.memory[x++] = bts2n('00010'); // nop #-121
-        this.memory[x++] = bts2n('iiiii'); // #
-
-        this.memory[x++] = bts2n('000i0'); // nop 29524
-        this.memory[x++] = bts2n('11111'); // xx
-        this.memory[x++] = bts2n('11111'); // xx
-
-        this.memory[x++] = bts2n('00011'); // bne, not taken
-        this.memory[x++] = bts2n('0000i'); //  relative branch destination, -1
-
-        this.memory[x++] = bts2n('00001'); // beq (br s=0,branch if sign trit flag is zero, accumulator is zero)
-        this.memory[x++] = bts2n('0001i'); //  relative branch destination, +2
-
-        this.memory[x++] = bts2n('iiiii'); // iiiii halt i, skipped by above branch
-        this.memory[x++] = bts2n('iii1i'); // iiiii halt 1, also skipped by same branch
-
-        this.memory[x++] = bts2n('1ii10'); // lda #
-        this.memory[x++] = bts2n('1iii0'); // #42
-
-        this.memory[x++] = bts2n('011i0'); // sta 0
-        this.memory[x++] = bts2n('00000'); // xx
-        this.memory[x++] = bts2n('00000'); // xx
-
-        this.memory[x++] = bts2n('iii0i'); // iiiii halt 0
-    */
-    ];
-
-    let machine_code = assembler(lines);
-    let i = 0;
-    for(let tryte of machine_code) {
-      this.memory[i++] = tryte;
-    }
-
-
     this.set_flag(FLAGS.F, -1); // fixed value
     this.set_flag(FLAGS.R, 1); // running: 1, program counter increments by; -1 runs backwards, 0 halts
 
     this.alu = new ALU(this);
 
     console.log('initial flags=',n2bts(this.flags));
+  }
+
+  assemble(source) {
+    let lines = source;
+    let machine_code = assembler(lines);
+    let i = 0; // TODO .org, offset to assemble into
+    for(let tryte of machine_code) {
+      this.memory[i++] = tryte;
+    }
   }
 
   // get a flag trit value given FLAGS.foo
--- demo.js
+++ demo.js
@@ -18,5 +18,63 @@ const term = Triterm({
   tritmap: cpu.memory.subarray(MEMORY_MAP.VIDEO_START, MEMORY_MAP.VIDEO_END)
 });
 
+var lines = [
+    'LDA #$ijk',
+    'LDA #%ii1i0',
+    'LDA #&QF',
+    'NOP A',
+    'NOP #-121',
+    'NOP 29524',
+    'LDA #0',
+    'BNE -1',
+    'BEQ +2',
+    'HALT_N',
+    'HALT_P',
+    'LDA #42',
+    'STA 0',
+
+    'LDA #%00i01',
+    'PTI A',
+
+    'TAX',
+
+    'HALT_Z'
+    /*
+
+      this.memory[x++] = bts2n('10i10'); // operation 10i, addressing mode 1
+      this.memory[x++] = bts2n('11001'); // flag 11, trit 0, compare 0
+      this.memory[x++] = bts2n('00000'); // nop a
+
+      this.memory[x++] = bts2n('00010'); // nop #-121
+      this.memory[x++] = bts2n('iiiii'); // #
+
+      this.memory[x++] = bts2n('000i0'); // nop 29524
+      this.memory[x++] = bts2n('11111'); // xx
+      this.memory[x++] = bts2n('11111'); // xx
+
+      this.memory[x++] = bts2n('00011'); // bne, not taken
+      this.memory[x++] = bts2n('0000i'); //  relative branch destination, -1
+
+      this.memory[x++] = bts2n('00001'); // beq (br s=0,branch if sign trit flag is zero, accumulator is zero)
+      this.memory[x++] = bts2n('0001i'); //  relative branch destination, +2
+
+      this.memory[x++] = bts2n('iiiii'); // iiiii halt i, skipped by above branch
+      this.memory[x++] = bts2n('iii1i'); // iiiii halt 1, also skipped by same branch
+
+      this.memory[x++] = bts2n('1ii10'); // lda #
+      this.memory[x++] = bts2n('1iii0'); // #42
+
+      this.memory[x++] = bts2n('011i0'); // sta 0
+      this.memory[x++] = bts2n('00000'); // xx
+      this.memory[x++] = bts2n('00000'); // xx
+
+      this.memory[x++] = bts2n('iii0i'); // iiiii halt 0
+  */
+  ];
+
+cpu.assemble(lines);
+
+term.tc.refresh();
+
 cpu.run();
 
